74f79ec1dfc347482b4f5d6b78d8fb88635610b9
uwulises <ulises.campodonico@ug.uchile.cl>
2021-11-03 16:36:36 -0300

    add serial_control

--- core/serial_control.py
+++ core/serial_control.py
@@ -1,17 +1,41 @@
-import serial
-
+from serial import Serial
+import time
 
 class Serial_control:
+
     def __init__(self, port="/dev/ttyUSB0"):
         self.port = port
+        self.serial = Serial
 
     def run_serial(self):
-        ser = serial
         try:
-            ser = serial.Serial(self.port, 115200, timeout=1)
+            self.serial = Serial(self.port, 115200, timeout=1)
             print("The port is available")
-
+            serial_port = "Open"
         except serial.serialutil.SerialException:
             print("The port is at use")
-            ser.close()
-            ser.open()
+            self.serial.close()
+            self.serial.open()
+
+    def close_Serial(self):
+        self.serial.close()
+
+    def write_servo(self, id, ang):
+        angleData = ang
+        if id == 1:
+            angleData = 2 * angleData
+        self.serial.write(('&' + str(id) + ':'+ str(angleData)).encode())
+        time.sleep(0.5)
+        print("listo")
+
+    def read_status(self):
+        status = "clean"
+        print(f"status: {status}")
+
+    def read_sensors(self):
+        status = "clean"
+        print(f"Sensor status: {status}")
+
+    def run_effector(self):
+        status = "ON"
+        print(f"Effector status: {status}")
