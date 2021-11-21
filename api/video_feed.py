41ca6ec0e93ab343fcacd5ebf351efcf2943c253
Francisco Casado <fcasado@ug.uchile.cl>
2021-11-21 02:38:09 -0300

    added single frame endpoint

--- api/video_feed.py
+++ api/video_feed.py
@@ -9,11 +9,19 @@ from mlf.core.camera import VideoStream
 class VideoFeed():
 
     def __init__(self):
-        self.video_stream = VideoStream(src=1)
+        self.video_stream = VideoStream(src=2)
 
     def gen(self):
+        """ Generator function (see more about `yield` documentation)
+        """
         while True:
             frame = self.video_stream.read()
             ret, jpeg = cv2.imencode('.jpg', frame)
             yield (b'--frame\r\n'
                 b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')
+    
+    def grab_single_frame(self):
+        frame = self.video_stream.read()
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        return jpeg.tobytes()
+
