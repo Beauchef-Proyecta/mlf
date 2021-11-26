043f482746c83c496918e0dc1834f68ab1bbef45
lukas <lpavezb>
2021-11-25 21:49:15 -0300

    add render template in / route

--- api/server.py
+++ api/server.py
@@ -1,9 +1,12 @@
+from flask import render_template
+
 from src import create_app
 
 app = create_app()
 
+@app.route('/')
+def index():
+    return render_template('index.html') 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=False)
-    
-
-
