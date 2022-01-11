0296a426300370e4a090c6251326d28e222e01cd
Francisco Casado <fcasado@ug.uchile.cl>
2022-01-11 13:50:17 -0300

    added base implementation for command_list (jump_list)

---
+++ arduino-firmware/MyLittleFactory.cpp
@@ -0,0 +1,109 @@
+
+/**
+Library for MK2Robot control and some effectors we use in The Little Factory
+see project repo @ https://github.com/Beauchef-Proyecta/mlf
+
+This library has been written following the Arduino documentation and guidelines
+https://www.arduino.cc/en/Hacking/LibraryTutorial
+https://www.arduino.cc/en/Reference/APIStyleGuide
+*/
+
+#include "MyLittleFactory.h"
+
+#include <Servo.h>
+
+Joint::Joint(){};
+
+Joint::Joint(int pin, int position) {
+    this->servo.attach(pin);
+    int params[] = {pin, position};
+    this->set_position(params);
+};
+
+int Joint::set_position(int *params) {
+    int val = params;
+    this->position = val;
+    this->servo.write(val);
+};
+
+Joint joints[3];
+
+int joint_set_position_wrapper(int *params) {
+    int id = params;
+    joints[id].set_position(params++);
+}
+
+/* Command List */
+
+func_ptr_t command_list[256];
+
+void build_command_list() {
+    command_list[0] = joint_set_position_wrapper;
+    command_list[1] = joint_set_position_wrapper;
+    command_list[2] = joint_set_position_wrapper;
+}
+
+/* Setup */
+void set_gpio() {
+    joints[0] = Joint(SERVO_J0, 0);
+    joints[1] = Joint(SERVO_J1, 0);
+    joints[2] = Joint(SERVO_J2, 0);
+}
+
+/* Homing */
+void axis_home() {}
+
+/* move_axis */
+void move_axis(int servoId, int position){
+    /*
+    if (servoId == 1) {
+      base.write(position);
+
+    }
+
+    if (servoId == 2) {
+      L1.write(position);
+
+    }
+
+    if (servoId == 3) {
+      L2.write(position);
+
+    }
+
+    if (servoId == 4) {
+      eff.write(position);
+
+    }
+    if (servoId == 5) {
+      if (position == 1) {
+        digitalWrite(pin_grip, HIGH);
+      }
+      if (position == 0) {
+        digitalWrite(pin_grip, LOW);
+      }
+    }
+    if (servoId == 6) {
+      if (position == 1) {
+        digitalWrite(pin_belt, HIGH);
+      }
+      if (position == 0) {
+        digitalWrite(pin_belt, LOW);
+      }
+    }
+    if (servoId == 7) {
+      if (position == 1) {
+        digitalWrite(pin_belt_turn, HIGH); //forward
+      }
+      if (position == 0) {
+        digitalWrite(pin_belt_turn, LOW); //backward
+      }
+    }
+    */
+
+};
+
+/* main */
+void do_everything(){
+
+};
---
+++ arduino-firmware/MyLittleFactory.h
@@ -0,0 +1,59 @@
+
+/** 
+Library for MK2Robot control and some effectors we use in The Little Factory
+see project repo @ https://github.com/Beauchef-Proyecta/mlf
+
+This library has been written following the Arduino documentation and guidelines
+https://www.arduino.cc/en/Hacking/LibraryTutorial
+https://www.arduino.cc/en/Reference/APIStyleGuide
+*/
+
+#include <Servo.h>
+
+
+/* GPIO DEFINITIONS */
+#define SERVO_J0 3
+#define SERVO_J1 5
+#define SERVO_J2 6
+#define GRIPPER_SERVO 9 // gripper
+#define GRIPPER_RELAY 7 //manage relay
+#define BELT_STATUS 8 //manage belt status
+#define BELT_DIRECTION 4 //manage belt forward-backward
+
+
+/* PROGRAM PARAMETERS */
+#define INPUT_SIZE 30
+
+
+/* HOME VALUES */
+#define HOME_J0 90
+#define HOME_J1 90
+#define HOME_J2 90
+#define HOME_GRIPPER 90
+
+
+
+class Joint{
+  private:
+    Servo servo;
+    int position;
+  
+  public:
+    Joint();
+    Joint(int pin, int position);
+
+    int set_position(int* params);
+};
+
+
+typedef int (*func_ptr_t)(int* );
+int f1( int );
+int f2( int );
+
+void set_gpio();
+
+void axis_home();
+
+void move_axis(int servoId, int position);
+
+void do_everything();
