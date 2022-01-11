228989575f4b6bd646cae58a59be2ac7ccf66e7d
Francisco Casado <fcasado@ug.uchile.cl>
2022-01-11 20:13:49 -0300

    test ready for transformations

--- tests/test_core_mk2robot.py
+++ tests/test_core_mk2robot.py
@@ -10,9 +10,60 @@ from core.mk2robot import (
 
 class TestTransformationMatrices(unittest.TestCase):
     def test_translation_along_zaxis(self):
-        p = translation_along_zaxis(0)
-        q = translation_along_zaxis(1)
-        np.testing.assert_array_equal(np.dot(p,q), q)
+        p = np.array([
+                [1, 0, 0, 0],
+                [0, 1, 0, 0],
+                [0, 0, 1, 0],
+                [0, 0, 0, 1]
+            ])
+
+        q = np.array([
+                [1, 0, 0, 0],
+                [0, 1, 0, 0],
+                [0, 0, 1, 1],
+                [0, 0, 0, 1]
+            ])
+
+        Tz = translation_along_zaxis(1)
+        np.testing.assert_array_equal(np.dot(p,Tz), q)
+
+    
+    def test_rotate_aroung_yaxis(self):
+        p = np.array([
+                [1, 0, 0, 0],
+                [0, 1, 0, 1],
+                [0, 0, 1, 1],
+                [0, 0, 0, 1]
+            ])
+
+        q = np.array([
+                [-1, 0, 0, 0],
+                [0, 1, 0, 1],
+                [0, 0, -1, -1],
+                [0, 0, 0, 1]
+            ])
+
+        Ry = rotation_around_yaxis(np.pi)
+        np.testing.assert_array_almost_equal(np.dot(Ry,p), q)
+
+
+    def test_rotate_aroung_zaxis(self):
+        p = np.array([
+                [1, 0, 0, 1],
+                [0, 1, 0, 1],
+                [0, 0, 1, 1],
+                [0, 0, 0, 1]
+            ])
+
+        q = np.array([
+                [-1, 0, 0, -1],
+                [0, -1, 0, -1],
+                [0, 0, 1, 1],
+                [0, 0, 0, 1]
+            ])
+
+        Rz = rotation_around_zaxis(np.pi)
+        np.testing.assert_array_almost_equal(np.dot(Rz,p), q)
 
 if __name__ == "__main__":
     unittest.main()
