80e15e674557e0ab39917dffdbc7654a9aced2ad
Francisco Casado <fcasado@ug.uchile.cl>
2022-01-21 16:16:24 -0300

    added camera source input parsing

--- examples/camera.py
+++ examples/camera.py
@@ -1,32 +1,40 @@
+import argparse
 import cv2
-import datetime
 import imutils
-import os
-import sys
-import time
 
-sys.path.insert(0, os.path.abspath('..'))
-from mlf.core.camera import VideoStream
+from core.vision.camera import VideoStream
 
-vs = VideoStream(src=1)
-vs.start()
-# loop over the frames from the video stream
-while True:
-	# grab the frame from the threaded video stream and resize it
-	# to have a maximum width of 400 pixels
-	frame = vs.read()
-	frame = imutils.resize(frame, width=400)
-	# draw the timestamp on the frame
-	timestamp = datetime.datetime.now()
-	ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
-	cv2.putText(frame, ts, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
-		0.35, (0, 0, 255), 1)
-	# show the frame
-	cv2.imshow("Frame", frame)
-	key = cv2.waitKey(1) & 0xFF
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
-# do a bit of cleanup
-cv2.destroyAllWindows()
-vs.stop()
+
+def parse_input():
+    parser = argparse.ArgumentParser(description="VideoStream Example.")
+    parser.add_argument(
+        "source",
+        type=int,
+        default=0,
+        nargs="?",
+        help="camera source, default 0",
+    )
+    return vars(parser.parse_args())
+
+
+def main():
+    src = parse_input()["source"]
+    vs = VideoStream(src=src)
+    vs.start()
+
+    while True:
+        frame = vs.read()
+        frame = imutils.resize(frame, width=400)
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
+
+        # if the `q` key was pressed, break from the loop
+        if key == ord("q"):
+            break
+
+    cv2.destroyAllWindows()
+    vs.stop()
+
+
+if __name__ == "__main__":
+    main()
