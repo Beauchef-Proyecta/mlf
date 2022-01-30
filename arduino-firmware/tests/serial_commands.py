f61551d31f5ff9f5f5d22d5e1d9f67bee95561cd
Francisco Casado <fcasado@ug.uchile.cl>
2022-01-30 04:59:38 -0300

    renamed file for preventing linting failure

--- arduino-firmware/tests/serial_commands.py
+++ arduino-firmware/tests/serial_commands.py
@@ -4,7 +4,7 @@ from core.serial import MK2Serial
 
 
 if __name__ == "__main__":
-    mk2 = SerialController()
+    mk2 = MK2Serial()
     mk2.open()
     i = 0
     while True:
@@ -14,6 +14,6 @@ if __name__ == "__main__":
         angle3 = int((30 * np.sin(i * np.pi / 180)) + 90) & 0xFF
         i %= 360
 
-        data = ser.build_serial_msg(0x10, [angle1, angle2, angle3])
-        s = ser.send_data(data)
+        data = mk2.build_serial_msg(0x10, [angle1, angle2, angle3])
+        s = mk2.send_data(data)
         print(f"[{i}: {angle1}]->{s}")
