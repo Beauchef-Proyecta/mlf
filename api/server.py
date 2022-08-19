f4fed8870d6c0faf019ea7f34877be3ab6ecaf2a
Francisco Casado Castro <hola@franciscocasado.cl>
2022-08-19 01:33:05 -0400

    feat(): add inverse kinematics when recieving input

--- api/server.py
+++ api/server.py
@@ -1,9 +1,11 @@
+import math
 from flask import Flask, request
 
 from core.model.mk2_robot import MK2Model
 
 app = Flask(__name__)
 mk2 = MK2Model()
+
 
 @app.route("/")
 def home():
@@ -18,13 +20,24 @@ def connect():
 @app.route("/move")
 def move_xyz():
 
-    x = request.args.get("x")
-    y = request.args.get("y")
-    z = request.args.get("z")
-    mk2.set_pose([int(x), int(y), int(z)])
+    x = float(request.args.get("x"))
+    y = float(request.args.get("y"))
+    z = float(request.args.get("z"))
+    q0, q1, q2 = mk2.inverse_kinematics((x, y, z))
+    mk2.set_pose(q0, q1, q2)
     pose = mk2.get_pose()
-    return f"Me moví a (x, y, x): ({pose})"
+    [x_new, y_new, z_new] = [int(a) for a in pose["link_4"][1]]
+
+    error = math.sqrt((x_new - x) ** 2 + (y_new - y) ** 2 + (z_new - z) ** 2)
+
+    if error >= 10:
+        return (
+            f"Parece que no puedo llegar a esa posición -- \n"
+            f"({x_new}, {y_new}, {z_new}) - error: {error}"
+        )
+
+    return f"Me moví a (x, y, x): {(x_new, y_new, x_new)}"
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=False)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
