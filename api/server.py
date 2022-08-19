19b8294dc8c173cc5838bfabbc8bdc5eebe760cc
Francisco Casado Castro <hola@franciscocasado.cl>
2022-08-19 00:54:44 -0400

    refactor(): remove video api

--- api/server.py
+++ api/server.py
@@ -1,12 +1,30 @@
-from flask import render_template
+from flask import Flask, request
 
-from .src import create_app
+from core.model.mk2_robot import MK2Model
 
-app = create_app()
+app = Flask(__name__)
+mk2 = MK2Model()
 
-@app.route('/')
-def index():
-    return render_template('index.html') 
+@app.route("/")
+def home():
+    return "Hello, World!"
+
+
+@app.route("/connect")
+def connect():
+    return "Connected!"
+
+
+@app.route("/move")
+def move_xyz():
+
+    x = request.args.get("x")
+    y = request.args.get("y")
+    z = request.args.get("z")
+    mk2.set_pose([int(x), int(y), int(z)])
+    pose = mk2.get_pose()
+    return f"Me moví a (x, y, x): ({pose})"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=False)
