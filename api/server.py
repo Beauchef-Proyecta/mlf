181b428056da0e14e29e37f6cb17889454c58a12
Francisco Casado Castro <hola@franciscocasado.cl>
2022-10-07 17:29:57 -0300

    try brute force camera feature

--- api/server.py
+++ api/server.py
@@ -1,14 +1,12 @@
-import math
-from flask import Flask, request, Response, Blueprint, make_response
+from flask import Flask, request, Response, render_template, make_response
+from api.camera import VideoCamera
 
 from core.model.mk2_robot import MK2Model
 from core.serial_wrapper.mk2_serial import MK2Serial
-from api.video_feed import VideoFeed
 
 app = Flask(__name__)
 mk2 = MK2Model()
 mk2_serial = MK2Serial()
-video_feed_builder = VideoFeed()
 
 
 @app.route("/")
@@ -40,19 +38,36 @@ def set_joints():
     return f"Mi nueva pose es: (q0={q0}, q1={q1}, q2={q2})"
 
 
-@app.route("/video_feed", methods=["GET"])
+pi_camera = VideoCamera(flip=False)  # flip pi camera if upside down.
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")  # you can customze index.html here
+
+
+def gen(camera):
+    # get camera frame
+    while True:
+        frame = camera.get_frame()
+        yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n\r\n")
+
+
+@app.route("/video_feed")
 def video_feed():
     return Response(
-        video_feed_builder.gen(), mimetype="multipart/x-mixed-replace; boundary=frame"
+        gen(pi_camera), mimetype="multipart/x-mixed-replace; boundary=frame"
     )
 
 
 @app.route("/get_frame", methods=["GET"])
 def single_frame():
-    response = make_response(video_feed_builder.grab_single_frame())
+    response = make_response(pi_camera.get_frame())
     response.headers["Content-Type"] = "image/jpg"
     return response
 
 
 if __name__ == "__main__":
+
     app.run(host="0.0.0.0", debug=True)
